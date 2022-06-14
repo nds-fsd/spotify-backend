0f2215e3920a27814398041f0db264be2519baa9
Mariela Morales <marielafmorales@gmail.com>
2022-06-14 08:03:21 +0200

    lista de canciones para carrusel ok

---
+++ src/songsList/songsList.js
@@ -0,0 +1,50 @@
+exports.songs[
+  ({
+    title: "Te Felicito",
+    duration: 189,
+    genre: "Pop",
+    releaseDate: "2022",
+  },
+  {
+    title: "Bonito",
+    duration: 246,
+    genre: "Rock en español",
+    releaseDate: "2003",
+  },
+  {
+    title: "Tu vida en la mía",
+    duration: 370,
+    genre: "Salsa",
+    releaseDate: "2019",
+  },
+  {
+    title: "Ojalá que llueva café",
+    duration: 240,
+    genre: "Merengue",
+    releaseDate: "1989",
+  },
+  {
+    title: "Perfect",
+    duration: 460,
+    genre: "Pop",
+    releaseDate: "2001",
+  },
+  {
+    title: "la bilirrubina",
+    duration: 240,
+    genre: "Merengue",
+    releaseDate: "1990",
+  },
+  {
+    title: "Just the way you are",
+    duration: 264,
+    genre: "Pop",
+    releaseDate: "2017",
+  },
+  {
+    title: "Everybody",
+    duration: 244,
+    genre: "Pop",
+    releaseDate: "1997",
+  })
+];
