42b6e53fd68bd25de08bacd5a87da95e2643d286
Mariela Morales <marielafmorales@gmail.com>
2022-06-17 07:17:10 +0200

    backend sprint1 done

--- src/mongo/Schema/songs.js
+++ src/mongo/Schema/songs.js
@@ -1,14 +1,13 @@
-
-
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
 const songsSchema = new Schema({
-    title: {type: String, required: true},
-    duration: {type: Number, required: true},
-    genre: {type: String, required: true},
-    releaseDate: {type: Date, required: true},
+  title: { type: String, required: true },
+  duration: { type: Number, required: true },
+  genre: { type: String, required: true },
+  releaseDate: { type: Date, required: true },
+  photo: { type: String, required: true },
 });
 
-const MySongs = model('songs', songsSchema);
+const MySongs = model("songs", songsSchema);
 
 module.exports = MySongs;
--- src/routers/music.js
+++ src/routers/music.js
@@ -1,72 +1,62 @@
-const express = require('express');
-const MySongs = require('../mongo/Schema/songs')
+const express = require("express");
+const MySongs = require("../mongo/Schema/songs");
 const songsRouter = express.Router();
 // const { songsList } = require('./dataSongs/songsList')
 
-
-songsRouter.get('/songs', async(req, res)=> {
-    const allSongs = await MySongs.find();
-    res.json(allSongs);
+songsRouter.get("/songs", async (req, res) => {
+  const allSongs = await MySongs.find();
+  res.json(allSongs);
 });
 
-
-songsRouter.get('/songs/:id', async(req, res) => {
-    const { id } = req.params;
-    if(id !== undefined){
-    const song = await MySongs.findById( id );
+songsRouter.get("/songs/:id", async (req, res) => {
+  const { id } = req.params;
+  if (id !== undefined) {
+    const song = await MySongs.findById(id);
     return res.json(song);
-}
+  }
 
-    return res.status(404).send();
-})
-
-
-
-
-
-songsRouter.post('/songs', async(req, res) => {
-    const body = req.body;
-
-    const data = {
-        title: body.title,
-        duration: body.duration,
-        genre: body.genre,
-        releaseDate: body.releaseDate
-    }
-
-    const newSong = MySongs(data);
-
-    await newSong.save();
-
-    res.json(newSong);
+  return res.status(404).send();
 });
 
+songsRouter.post("/songs", async (req, res) => {
+  const body = req.body;
 
+  const data = {
+    title: body.title,
+    duration: body.duration,
+    genre: body.genre,
+    releaseDate: body.releaseDate,
+    photo: body.photo,
+  };
 
-songsRouter.patch('/songs/:id', async(req, res) => {
-    const { id } = req.params;
-    const { body } = req;
-    if(id !== undefined){
-    const song = await MySongs.findOneAndUpdate({ _id: id }, body, { new: true });
-    
+  const newSong = MySongs(data);
+
+  await newSong.save();
+
+  res.json(newSong);
+});
+
+songsRouter.patch("/songs/:id", async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  if (id !== undefined) {
+    const song = await MySongs.findOneAndUpdate({ _id: id }, body, {
+      new: true,
+    });
+
     return res.json(song);
-    }
+  }
 
-    return res.status(404).send(); 
-})
+  return res.status(404).send();
+});
 
-songsRouter.delete('/songs/:id', async(req, res) => {
-    
-    
-    const song = await MySongs.findByIdAndRemove( req.params.id );
+songsRouter.delete("/songs/:id", async (req, res) => {
+  const song = await MySongs.findByIdAndRemove(req.params.id);
 
-    return res.status(200).send( {message: "Song Deleted"} );
-
-})
+  return res.status(200).send({ message: "Song Deleted" });
+});
 
 //me elimina el primer elemento de mi bbdd
 //preguntar findByIdAndDelete y findOneAndDelete
-
-
 
 module.exports = songsRouter;
