293e24021d7a167a6fb0ad4929cc8d10075fd2f3
Miquel Villanueva <mivima86@gmail.com>
2022-07-13 11:19:16 +0200

    falla relacion base de datos

--- src/mongo/Schema/Song/songs.js
+++ src/mongo/Schema/Song/songs.js
@@ -3,14 +3,19 @@ const Artists = require("../Artists/artists");
 
 
 const songsSchema = new Schema({
-  title: { type: String, required: true },
-  duration: { type: Number, required: true },
-  genre: { type: String, required: true },
-  releaseDate: { type: Date, required: true },
-  photo: { type: String, required: true },
-  artists: { type: Schema.Types.ObjectId,
-  ref: "artists",
-  required: true 
+  title: { 
+    type: String, required: true },
+  duration: { 
+    type: Number, required: true },
+  genre: { 
+    type: String, required: true },
+  releaseDate: { 
+    type: Date, required: true },
+  photo: { 
+    type: String, required: true },
+  artists: { 
+    type: Schema.Types.ObjectId,
+   ref: "artists", //required: true 
 }
 });
 
