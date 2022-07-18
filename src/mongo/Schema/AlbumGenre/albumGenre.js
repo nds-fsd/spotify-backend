dc2a8e8edfc327d4f29699430f4bdee6dbdfb529
Diogo Santos <diogo.nogal.santos@gmail.com>
2022-07-18 20:06:51 +0200

    role-admin

--- src/mongo/Schema/AlbumGenre/albumGenre.js
+++ src/mongo/Schema/AlbumGenre/albumGenre.js
@@ -3,6 +3,12 @@ const { Schema, model } = require("mongoose");
 const albumGenreSchema = new Schema({
   Album: { type: id, rquire: true },
   Genre: { type: id, require: true },
+  role: {
+    type: String,
+    enum: ["ADMIN", "USER"],
+    default: "ADMIN",
+    required: true,
+  },
 });
 
 const AlbumGenre = model("albumGenre", albumGenreSchema);
