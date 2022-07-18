edbe5bb74f6ea4c688805ba2d1f933f0be69a0c6
Diogo Santos <diogo.nogal.santos@gmail.com>
2022-07-18 20:09:11 +0200

    undo admin

--- src/mongo/Schema/Genre/genre.js
+++ src/mongo/Schema/Genre/genre.js
@@ -4,12 +4,6 @@ const genreSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, require: true },
   photo: { type: String, required: true },
-  role: {
-    type: String,
-    enum: ["ADMIN", "USER"],
-    default: "ADMIN",
-    required: true,
-  },
 });
 
 const Genre = model("genre", genreSchema);
