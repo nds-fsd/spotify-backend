4473da29ae428fa2cd31cf8155f5fdfc6402a3d8
Mariela Morales <marielafmorales@gmail.com>
2022-09-07 18:43:26 +0200

    back ready to merge sprint5

--- src/mongo/Schema/Genre/genre.js
+++ src/mongo/Schema/Genre/genre.js
@@ -4,6 +4,7 @@ const genreSchema = new Schema({
   name: { type: String, required: true },
   description: { type: String, require: true },
   photo: { type: String, required: true },
+  song: [{ type: Schema.Types.ObjectId, ref: "song" }],
 });
 
 const Genre = model("genre", genreSchema);
